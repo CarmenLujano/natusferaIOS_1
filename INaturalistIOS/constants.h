1236d813951770c6be103b2bf5fd444b0e068cfd
SaMa <sama@gbif.es>
2016-11-21 14:49:27 +0100

    código funcionando sin el sign in

--- INaturalistIOS/constants.h
+++ INaturalistIOS/constants.h
@@ -21,9 +21,9 @@
 
 
 #ifdef DEBUG1
-    #define INatBaseURL @"http://localhost:3000"
-    #define INatMediaBaseURL @"http://127.0.0.1:3000"
-    #define INatWebBaseURL @"http://127.0.0.1:3000"
+    #define INatBaseURL @"http://natusfera.gbif.es"
+    #define INatMediaBaseURL @"http://natusfera.gbif.es"
+    #define INatWebBaseURL @"http://natusfera.gbif.es"
 #else
     // base URL for all API requests
     #define INatBaseURL @"http://natusfera.gbif.es"
